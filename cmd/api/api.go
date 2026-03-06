26d95b77169789b055841eaca795593b14251998
Vikas Singh <33988028+xanderbilla@users.noreply.github.com>
2026-03-06 08:41:28 +0000

    feat(setup): Implement basic API server with health check endpoint
    - Added main application structure and server configuration in cmd/api/api.go
    - Implemented health check handler in cmd/api/health.go
    - Created main entry point for the application in cmd/api/main.go
    - Included chi router for handling HTTP requests
    - Configured middleware for logging, recovery, and timeouts
    - Updated go.sum to include chi dependency

---
+++ cmd/api/api.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
+}
+
+func (app *application) run(mux http.Handler) error {
+
+	// confiure the server
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server started at port%s ...", app.config.addr)
+
+	// start the server (starting point)
+	return srv.ListenAndServe()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module gituhub.com/xanderbilla/my-project
+module github.com/xanderbilla/my-project
 
 go 1.25.4
+
+require github.com/go-chi/chi/v5 v5.2.5 // indirect
